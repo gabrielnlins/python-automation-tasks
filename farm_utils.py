86e8043e0a78341e84915404d2f05ed702bc157c
gnilo <gabrielnilolins@hotmail.com>
2025-03-30 12:54:24 -0300

    refactor: removes unused functions

--- farm_utils.py
+++ farm_utils.py
@@ -9,11 +9,11 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\tesseract.exe"
 tessdata_dir_config = r'--tessdata-dir "C:\Program Files\Tesseract-OCR\tessdata"'
 
 solve_challenge = 'resolva o desafio'
-confirmation_button_path = r"C:\Users\Gabri\game\src\python_automation_tasks\images\confirm_button.png"
-verify_left_arrow = r"C:\Users\Gabri\game\src\python_automation_tasks\images\left_arrow_2.png"
-avoid = r"C:\Users\Gabri\game\src\python_automation_tasks\images\avoid.png"
-# attack_button = r"C:\Users\Gabri\game\src\python_automation_tasks\images\start.png"
-# stop_button = r"C:\Users\Gabri\game\src\python_automation_tasks\images\stop.png"
+confirmation_button_path = r"C:\Users\Gabri\game\src\python-automation-tasks\images\confirm_button.png"
+verify_left_arrow = r"C:\Users\Gabri\game\src\python-automation-tasks\images\left_arrow_2.png"
+avoid = r"C:\Users\Gabri\game\src\python-automation-tasks\images\avoid.png"
+# attack_button = r"C:\Users\Gabri\game\src\python-automation-tasks\images\start.png"
+# stop_button = r"C:\Users\Gabri\game\src\python-automation-tasks\images\stop.png"
 
 timeout = 60
 middle_screen = 1566,320
@@ -26,9 +26,10 @@ count = 0
 
 def click():
     pyautogui.click(middle_screen, duration=0)
+    print(f"pyautogui position: {pyautogui.position()}")
 
 def reset():
-    # click()
+    click()
     stop_attack()
     write_message('/resetar')
     sleep(1)
@@ -56,7 +57,6 @@ def hold_button(x, y):
 
 def farm(location):
     sleep(1)
-    # data = get_data()
     start_time = time()
     while not get_images():
         print(f"Executando o loop de farm()... start_time: {start_time}, timeout: {timeout}")
@@ -86,19 +86,16 @@ def farm(location):
 def stop_attack():
     try:
         pyautogui.click(attack_button, duration=0)
-        # button = pyautogui.locateOnScreen(stop_button, confidence=0.8)
-        # pyautogui.click(pyautogui.center(button))
     except Exception as e:
         print(f"Erro ao procurar botão de STOP {e}")
         return True
 def start_attack():
     try:
         pyautogui.click(attack_button, duration=0)
-        # button = pyautogui.locateOnScreen(attack_button, confidence=0.8)
-        # pyautogui.click(pyautogui.center(button))
     except Exception as e:
         print(f"Erro ao procurar botão de START {e}")
         return True
+
 def auto_farm():
     while True:
         reset()
@@ -110,8 +107,7 @@ def after_mr():
         farm('/losttower7')
 
 def get_images():
-    # click()
-    clicar_na_janela_virtualmente("MudinhoX", 1566, 320)
+    click()
     try:
         text = pytesseract.image_to_string(Image.open(avoid), lang="eng")
         coord_images = pyautogui.locateOnScreen(verify_left_arrow, confidence=0.8)
@@ -125,8 +121,6 @@ def get_images():
                 sleep(3)
                 print("Imagem encontrada...")
                 click_on_confirm()
-                # button = pyautogui.locateOnScreen(confirmation_button_path, confidence=0.8)
-                # pyautogui.click(pyautogui.center(button))
                 sleep(3)
                 return True
         else:
@@ -150,25 +144,24 @@ def rotate_image(image_path):
     if "left_arrow" in image_path:
         button = pyautogui.locateOnScreen(verify_left_arrow, confidence=0.8)
         pyautogui.click(pyautogui.center(button))
-        add_retry()
 
 def find_correct_image(count):
     images_to_check = [
-        r"C:\Users\Gabri\game\src\python_automation_tasks\images_on_format\axe_1.png",
-        r"C:\Users\Gabri\game\src\python_automation_tasks\images_on_format\bull_1.png",
-        r"C:\Users\Gabri\game\src\python_automation_tasks\images_on_format\dragon_armor.png",
-        r"C:\Users\Gabri\game\src\python_automation_tasks\images_on_format\dragon_helmet.png",
-        r"C:\Users\Gabri\game\src\python_automation_tasks\images_on_format\boots_3.png",
-        r"C:\Users\Gabri\game\src\python_automation_tasks\images_on_format\boots_1.png",
-        r"C:\Users\Gabri\game\src\python_automation_tasks\images_on_format\boots_2.png",
-        r"C:\Users\Gabri\game\src\python_automation_tasks\images_on_format\brass_armor_2.png",
-        r"C:\Users\Gabri\game\src\python_automation_tasks\images_on_format\iron_shield_1.png",
-        r"C:\Users\Gabri\game\src\python_automation_tasks\images_on_format\scale_helmet.png",
-        r"C:\Users\Gabri\game\src\python_automation_tasks\images_on_format\skull_shield.png",
-        r"C:\Users\Gabri\game\src\python_automation_tasks\images_on_format\sm_armor.png",
-        r"C:\Users\Gabri\game\src\python_automation_tasks\images_on_format\small_shield.png",
-        r"C:\Users\Gabri\game\src\python_automation_tasks\images_on_format\sorcerer.png",
-        r"C:\Users\Gabri\game\src\python_automation_tasks\images_on_format\spider.png",
+        r"C:\Users\Gabri\game\src\python-automation-tasks\images_on_format\axe_1.png",
+        r"C:\Users\Gabri\game\src\python-automation-tasks\images_on_format\bull_1.png",
+        r"C:\Users\Gabri\game\src\python-automation-tasks\images_on_format\dragon_armor.png",
+        r"C:\Users\Gabri\game\src\python-automation-tasks\images_on_format\dragon_helmet.png",
+        r"C:\Users\Gabri\game\src\python-automation-tasks\images_on_format\boots_3.png",
+        r"C:\Users\Gabri\game\src\python-automation-tasks\images_on_format\boots_1.png",
+        r"C:\Users\Gabri\game\src\python-automation-tasks\images_on_format\boots_2.png",
+        r"C:\Users\Gabri\game\src\python-automation-tasks\images_on_format\brass_armor_2.png",
+        r"C:\Users\Gabri\game\src\python-automation-tasks\images_on_format\iron_shield_1.png",
+        r"C:\Users\Gabri\game\src\python-automation-tasks\images_on_format\scale_helmet.png",
+        r"C:\Users\Gabri\game\src\python-automation-tasks\images_on_format\skull_shield.png",
+        r"C:\Users\Gabri\game\src\python-automation-tasks\images_on_format\sm_armor.png",
+        r"C:\Users\Gabri\game\src\python-automation-tasks\images_on_format\small_shield.png",
+        r"C:\Users\Gabri\game\src\python-automation-tasks\images_on_format\sorcerer.png",
+        r"C:\Users\Gabri\game\src\python-automation-tasks\images_on_format\spider.png",
     ]
     try:
         for images in images_to_check:
@@ -191,32 +184,16 @@ def find_correct_image(count):
         print(f"An error occurred: {e} while looking for: {end}")
         return False
 
-def add_retry():
-    data = get_data()
-    data['count_retry'] = data['count_retry'] + 1
-    set_data(data)
-    return data
-
-def is_possible_retry(data):
-    if data['count_retry'] > 5: #30
-        print("Retorna True para evitar loop infinito.")
-        return False
-    else:
-        return True
-
 def get_data():
-    with open(r"C:\Users\Gabri\game\src\python_automation_tasks\database.json", 'r') as file:
+    with open(r"C:\Users\Gabri\game\src\python-automation-tasks\database.json", 'r') as file:
         return json.load(file)
 
 def set_data(data):
-    with open(r"C:\Users\Gabri\game\src\python_automation_tasks\database.json", 'w') as file:
+    with open(r"C:\Users\Gabri\game\src\python-automation-tasks\database.json", 'w') as file:
         json.dump(data, file, indent=4)
 
-
-
-
 def tirar_print_janela(nome_janela, caminho_arquivo):
-    # click()
+    #click()
     janela = gw.getWindowsWithTitle(nome_janela)[0]
     janela.activate()
     print(f"Janela encontrada: {janela.title}")
@@ -226,7 +203,6 @@ def tirar_print_janela(nome_janela, caminho_arquivo):
     print(f"Janela encontrada: {janela.title}")
     print(f"Coordenadas: ({x}, {y}), Tamanho: {largura}x{altura}")
 
-    # Tira o print apenas da região da janela
     screenshot = pyautogui.screenshot(region=(x, y, largura, altura))
     screenshot.save(caminho_arquivo)
     print(f"Screenshot salvo em: {caminho_arquivo}")
@@ -242,46 +218,3 @@ def read_properties(file_path):
             full_list.append(string)
     print(f"Quantidade de atributos full: {len(full_list)}")
     return len(full_list)
-
-def clicar_na_janela(nome_janela, x_relativo, y_relativo):
-    # Localiza a janela pelo título
-    janela = gw.getWindowsWithTitle(nome_janela)[0]
-    if janela:
-        # Calcula as coordenadas absolutas com base na posição da janela
-        x_absoluto = janela.left + x_relativo
-        y_absoluto = janela.top + y_relativo
-
-        # Simula o clique nas coordenadas calculadas
-        pyautogui.click(x=x_absoluto, y=y_absoluto)
-        print(f"Clique realizado em ({x_absoluto}, {y_absoluto}) na janela '{nome_janela}'")
-    else:
-        print(f"Janela '{nome_janela}' não encontrada.")
-
-import win32gui
-import win32api
-import win32con
-
-def clicar_na_janela_virtualmente(nome_janela, x_relativo, y_relativo):
-    # Localiza a janela pelo título
-    hwnd = win32gui.FindWindow(None, nome_janela)
-    if hwnd:
-        # Obtém as coordenadas da janela
-        left, top, right, bottom = win32gui.GetWindowRect(hwnd)
-        # x_absoluto = left + x_relativo
-        # y_absoluto = top + y_relativo
-        x_absoluto = x_relativo
-        y_absoluto = y_relativo
-
-        # Envia um evento de clique para a janela
-        lParam = win32api.MAKELONG(x_absoluto, y_absoluto)
-        win32api.SendMessage(hwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, lParam)
-        win32api.SendMessage(hwnd, win32con.WM_LBUTTONUP, None, lParam)
-        print(f"Clique virtual realizado em ({x_absoluto}, {y_absoluto}) na janela '{nome_janela}'")
-    else:
-        print(f"Janela '{nome_janela}' não encontrada.")
-
-# Exemplo de uso
-# clicar_na_janela_virtualmente("MudinhoX", 1566, 320)  # Clique em (100, 200) relativo à janela
-
-# Exemplo de uso
-# clicar_na_janela("MudinhoX", 100, 200)  # Clique em (100, 200) relativo à janela
--- index.py
+++ index.py
@@ -1,43 +1,6 @@
-from farm_utils import auto_farm, after_mr, get_images, tirar_print_janela
-import pygetwindow as gw
-# encontrar_texto_na_tela
+from farm_utils import auto_farm, after_mr
 
-auto_farm()
+# auto_farm()
 
-# after_mr()
-# Chamando a função
-# get_images(0)
+after_mr()
 
-# janelas = gw.getAllTitles()
-
-# Exibe as janelas abertas
-# print("Janelas abertas:")
-# for janela in janelas:
-#     print(janela)
-
-# tirar_print_janela("MudinhoX", r"C:\Users\Gabri\game\src\auto_farm\mudinho_screenshot.png")
-
-# for janela in gw.getAllWindows():
-#     if nome_janela in janela.title:
-#         janela_mudinho = janela
-#         break
-
-# if janela_mudinho:
-#     print(f"Janela encontrada: {janela_mudinho.title}")
-#     print(f"Coordenadas: ({janela_mudinho.left}, {janela_mudinho.top})")
-#     print(f"Tamanho: {janela_mudinho.width}x{janela_mudinho.height}")
-# else:
-#     print(f"Janela '{nome_janela}' não encontrada.")
-
-
-
-
-
-
-# TODO: tirar print dos status full para automatizar o MR.
-
-# resultado = encontrar_texto_na_tela("Level")
-# if resultado["encontrado"]:
-#     print(f'Texto encontrado: {resultado["texto"]}')
-# else:
-#     print(f'Texto não encontrado. Texto extraído: {resultado["texto"]}')
